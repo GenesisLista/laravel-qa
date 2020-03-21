0ffc321747b81521eb8ce7a3066e540eb6739d2b
Genesis Lista <genesis.bergonia.lista@gmail.com>
2020-03-21 10:52:14 +0300

    Display all questions

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/* Resource because it will have CRUD */
+Route::resource('questions', 'QuestionsController');
