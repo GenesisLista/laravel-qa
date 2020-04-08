a0c22644ace8605cdb571b5d1f9a253de35a421c
Genesis Lista <genesis.bergonia.lista@gmail.com>
2020-04-08 21:19:57 +0300

    Display answers for questions

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 /* Resource because it will have CRUD */
 Route::resource('questions', 'QuestionsController')->except('show');
+//Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
+Route::resource('questions.answers', 'AnswersController')->only(['store', 'edit', 'update', 'destroy']);
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
